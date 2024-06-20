30b140ba7dd7e13a4e6877eb4164d1f798f02e7a
acceleratorusa <acceleratorusa@gmail.com>
2024-06-21 03:29:32 +0500

    coca cola

--- api/index.js
+++ api/index.js
@@ -2,11 +2,7 @@ const bot = require("../bot");
 
 module.exports = async (req, res) => {
   try {
-    if (req.method === "POST") {
-      await bot(req, res);
-    } else {
-      res.status(200).send("OK");
-    }
+    await bot(req, res);
   } catch (error) {
     res.statusCode = 500;
     res.setHeader("Content-Type", "text/html");
